1019731fd5ebda45887e5a427ea370f43cc3631f
himanshu-rathore-5exe <himanshu..rathore@5exceptions.com>
2025-03-24 17:49:06 +0530

    fix

--- ios/Sources/TruvideoSdkCameraPlugin/TruvideoSdkCameraPlugin.swift
+++ ios/Sources/TruvideoSdkCameraPlugin/TruvideoSdkCameraPlugin.swift
@@ -1,5 +1,6 @@
 import Foundation
 import Capacitor
+import TruvideoSdkCamera
 
 /**
  * Please read the Capacitor iOS Plugin Development Guide
@@ -9,9 +10,12 @@ import Capacitor
 public class TruvideoSdkCameraPlugin: CAPPlugin, CAPBridgedPlugin {
     public let identifier = "TruvideoSdkCameraPlugin"
     public let jsName = "TruvideoSdkCamera"
+    
     public let pluginMethods: [CAPPluginMethod] = [
-        CAPPluginMethod(name: "echo", returnType: CAPPluginReturnPromise)
+        CAPPluginMethod(name: "echo", returnType: CAPPluginReturnPromise),
+        CAPPluginMethod(name: "initCameraScreen", returnType: CAPPluginReturnPromise) // Registering initCameraScreen
     ]
+
     private let implementation = TruvideoSdkCamera()
 
     @objc func echo(_ call: CAPPluginCall) {
@@ -19,5 +23,44 @@ public class TruvideoSdkCameraPlugin: CAPPlugin, CAPBridgedPlugin {
         call.resolve([
             "value": implementation.echo(value)
         ])
+    }
+
+
+ /**
+     Initiates the camera functionality with specified parameters and presents the camera view.
+     - Parameters:
+        - viewController: The UIViewController or SwiftUIView where the camera view will be presented.
+        - completion: A closure to handle the camera result upon completion.
+            - cameraResult: The result of the camera operation.
+     */
+    @obj func initCameraScreen(viewController: UIViewController, _ completion: @escaping (_ cameraResult: TruvideoSdkCameraResult) -> Void) {
+        DispatchQueue.main.async {
+            // Retrieving information about the device's camera functionality.
+            let cameraInfo: TruvideoSdkCameraInformation = TruvideoSdkCamera.camera.getTruvideoSdkCameraInformation()
+            
+            // Configuring the camera with various parameters based on specific requirements.
+            // - Parameter outputPath: Specifies the path for storing the captured data upon completion.
+            // Additional parameters can be adjusted to customize camera behavior as needed.
+            let configuration = TruvideoSdkCameraConfiguration(
+                lensFacing: .back,
+                flashMode: .off,
+                orientation: nil,
+                outputPath: "",
+                frontResolutions: [],
+                frontResolution: nil,
+                backResolutions: [],
+                backResolution: nil,
+                mode: .videoAndPicture()
+            )
+            
+            DispatchQueue.main.async {
+                viewController.presentTruvideoSdkCameraView(
+                    preset: configuration,
+                    onComplete: { cameraResult in
+                        // Handling completion of camera
+                    }
+                )
+            }
+        }
     }
 }
