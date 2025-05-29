d27ce52e1e9e0f4c42da2b8e7f7fdfe10fdefae8
himanshu-rathore-5exe <himanshu.rathore@5exceptions.com>
2025-05-29 20:43:34 +0530

    fix

--- dist/esm/web.d.ts
+++
@@ -1,9 +0,0 @@
-import { WebPlugin } from '@capacitor/core';
-import type { CameraPluginPlugin } from './definitions';
-export declare class CameraPluginWeb extends WebPlugin implements CameraPluginPlugin {
-    echo(options: {
-        value: string;
-    }): Promise<{
-        value: string;
-    }>;
-}
